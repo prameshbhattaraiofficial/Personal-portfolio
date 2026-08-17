0d078a050b9128148ac78e48a626b11305b8887c
prameshbhattaraiofficial <prameshbhattaraiofficial@gmail.com>
2026-08-17 17:29:41 +0545

    frontent change

--- personalportfolio.client/src/components/Nav.jsx
+++ personalportfolio.client/src/components/Nav.jsx
@@ -3,7 +3,7 @@ export default function Nav() {
     <header className="nav">
       <div className="nav-inner">
         <div className="nav-logo">
-          pramesh<span>.dev</span>
+          Pramesh<span>Bhattarai</span>
         </div>
         <ul className="nav-links">
           <li><a href="#summary">// summary</a></li>
