7bfeec19259144a983909556467a5702ebd13c06
prameshbhattaraiofficial <prameshbhattaraiofficial@gmail.com>
2026-08-18 19:50:11 +0545

    All set

--- personalportfolio.Server/obj/Debug/net8.0/personalportfolio.Server.AssemblyInfo.cs
+++ personalportfolio.Server/obj/Debug/net8.0/personalportfolio.Server.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("personalportfolio.Server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3b1ffd2322fb28e4be70d9a01478a33495224d9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6cf9373022c95031d75b0acfb8ae7d18567b16c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("personalportfolio.Server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("personalportfolio.Server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
